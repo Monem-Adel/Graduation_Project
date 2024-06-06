efbf0ceed7b3bd32923865a029aab4150a1db5c6
PhilopateerMaged02 <153292805+PhilopateerMaged02@users.noreply.github.com>
2024-06-06 23:18:53 +0300

    Add files via upload

--- Scanner/Scanner.py
+++ Scanner/Scanner.py
@@ -1,40 +1,21 @@
-# this file for connecting classification with automaton (scanner)
-# بص هنحط كله ف فنكشنز وفي واحده منهم هترجع اوتوماتون بعد ما يتملي
-# ونظبط االسورس والجداول (او هترجع الترنزيشن تيبول)
-# والفنكشن ده هتتناده في البرسر 
-# two functions one for scanner & another for parser
 import sys
-# sys.path.append(r"D:\\3loom\\4thYear\\2ndSemester\\GraduationProject\\Graduation_Project\\Image_Processing")
-# sys.path.append(r"D:\\3loom\\4thYear\\2ndSemester\\GraduationProject\\Graduation_Project")
-sys.path.append(r"H:\Graduation Project\Graduation_Project\Parser\Parser")
-sys.path.append(r"H:\Graduation Project\Graduation_Project\Image_Processing")
-
-# print(sys.path)
+sys.path.append(r"D:\4th\Second Sem\Graduation Project\Graduation_Project\Parser\Parser")
+sys.path.append(r"D:\4th\Second Sem\Graduation Project\Graduation_Project\Image_Processing")
 
 import Automaton
 import State
 import Transition
 import Arrow
 import Models
-import Parser
+import parser
 
-# method to take a path of image from the user
 def take_pic():
-    ##### BASSAM #####
-    # you can change this method or link it to the flutter
-    img_path = input("Enter your image path")
-    img_path.lower().strip()
+
+    img_path = input("Enter your image path").lower().strip()
     return img_path
 
-
-# img_path = 'H:\Graduation Project\Graduation_Project\Image_Processing\jflab\jflab2.png'
-# img_path = "D:\\3loom\\4thYear\\2ndSemester\\GraduationProject\\Graduation_Project\\Scanner\\test.png"
-
-# (1st) method to scan the image & initialize the automaton (scanner method called once in the program)
 def scan_auto(img_path):
-
-    # assign classified objects into lists & traversing them to create objects from specified classes
-
+    
     listOfDict_states = Models.get_classified_States(img_path)
     listOfDict_trans = Models.get_classified_transitions(img_path)
 
@@ -42,80 +23,65 @@ def scan_auto(img_path):
     scannered_trans = []
     scannered_labels = set()
 
-    # the dict of classified states is :-
-    # {'type': 'Final_State', 'bbox': (362, 130, 393, 197), 'name': ''}
-    # t => type
-    # bb => bbox
-    # n => name
     
     for state in listOfDict_states:
         t = state['type']
-        if(t == 'Start_State'):
+        if t == 'Start_State':
             t = State.Type_of_state.Start_State
-        elif(t == 'State'):
+        elif t == 'State':
             t = State.Type_of_state.Normal_State
-        elif(t == 'Final_State'):
+        elif t == 'Final_State':
             t = State.Type_of_state.Final_State
         bb = state['bbox']
         n = state['name']
+        scannered_states.append(State.state(n, t, bb))
 
-        scannered_states.append(State.state(n,t,bb))
-
-    # the dict of classified transitions is :-
-    # {'type': 'Transition', 'bbox': (141, 241, 242, 262), 'Label': '2 4', 'Direction': 'left arrow'}
-    # d => direction
-    # bb => bbox
-    # l => label
-
+    
     for trans in listOfDict_trans:
-        if(trans['type'] == 'Loop'):
+        if trans['type'] == 'Loop':
             d = Transition.Arrow.Direction.Loop
         else:
             d = trans['Direction']
-            if(d == 'Left'):
+            if d == 'Left':
                 d = Transition.Arrow.Direction.Left
-            elif (d =="Right"):
+            elif d == 'Right':
                 d = Transition.Arrow.Direction.Right
-            elif (d =="Up"):
+            elif d == 'UP':
                 d = Transition.Arrow.Direction.Up
-            elif (d =="Down"):
+            elif d == 'Down':
                 d = Transition.Arrow.Direction.Down
+            else:
+                raise ValueError(f"Unknown direction: {d}")
+
         bb = trans['bbox']
         l = trans['Label']
 
         scannered_labels.add(l)
-            
-        scannered_trans.append(Transition.transition(l,bb,Arrow.arrow(direction = d)))
+        scannered_trans.append(Transition.transition(l, bb, Arrow.arrow(direction=d)))
 
-    # create an automaton
-    auto = Automaton.automaton(scannered_states,scannered_trans,scannered_labels,img_path)
+    
+    auto = Automaton.automaton(scannered_states, scannered_trans, scannered_labels, img_path)
 
+    
     table = auto.make_transition_table()
     start_state = auto.get_start_state()
 
-    # return tuple (automaton object, transition table, start state)
-    return (auto,table,start_state)
+    
+    return (auto, table, start_state)
 
-# print(scan_auto(img_path))
+def parse_auto(start_state, transition_table, test_cases):
 
-# (2nd) method to parse the automaton 
-def parse_auto(start_state, transition_table: list,test_cases: list):
-    parser  = Parser.parser(start_state, transition_table)
-    results = parser.test_strings(test_cases)
+    parser_obj = parser.parser(start_state, transition_table)
+    results = parser_obj.test_strings(test_cases)
     return results
 
-
 def test_all():
-    test_cases = ["00", "1001"]
-    # image_path = "D:\\3loom\\4thYear\\2ndSemester\\GraduationProject\\Graduation_Project\\Scanner\\test.png"
-    image_path = take_pic()
-    image_path.lower().strip()
+    test_cases = ["0011", "1001"]
+    image_pathh = "D:\\4th\\Second Sem\\Graduation Project\\Graduation_Project\\Image_Processing\\auto_2.jpg"
+    image_path = image_pathh.lower().strip()
     automaton, transition_table, start_state = scan_auto(image_path)
     results = parse_auto(start_state, transition_table, test_cases)
-    i = 0
-    for result in results:
-        print(f"result for test case no {i} is {result}")
-        i = i+1
+    for i, result in enumerate(results):
+        print(f"Result for test case no {i} is {result}")
 
 test_all()
-
