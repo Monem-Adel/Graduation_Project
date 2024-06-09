6adadcee99368799f0ba667b018ed0228427100c
Bassam ElArabi <68780788+bassamelarabi@users.noreply.github.com>
2024-06-09 19:46:43 +0300

    Create API file and adding app.py

---
+++ API/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify, request
+import os
+import werkzeug
+import werkzeug.utils
+
+app = Flask(__name__)
+
+
+@app.route('/api', methods=['POST'])
+def evaluate():
+    if request.method == "POST":
+        # the following part is to recieve the image from flutter app request and save it in Automatonimage folder
+        image_file = request.files['image']
+        file_name = werkzeug.utils.secure_filename(image_file.filename)
+        image_file.save("./AutomatonImage/"+file_name)
+        # now, recieving a string
+        sl = str (request.form['str'])
+        #res = evaluate(sl) // later, to test function call on the input and get the results 
+        
+        return jsonify({'result':'the image has been recieved succefully and the string sent is'+'  '+sl})
+
+
+if __name__ == "__main__":
+    app.run()
